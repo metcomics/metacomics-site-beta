8cda9d0793a0a80fca8716e05f4465df0e030c8c
Metacomics <metatirinhas@gmail.com>
2026-07-09 16:03:53 -0300

    arquivos iniciais

---
+++ JS/comics-data.js
@@ -0,0 +1,154 @@
+/* ==============================================================
+   BANCO DE DADOS DE WEBCOMICS
+   Adicione, edite ou remova as suas HQs nesta lista.
+   ============================================================== */
+
+const comicsData = [
+    { 
+        id: 1, 
+        title: "Som Animado", 
+        author: "Pâmela de Castro", 
+        category: "bilu e varginha", 
+        color: "00E5FF", 
+        action: "GLITCH!", 
+        synopsis: "Uma tirinha musical, onde Bilu e Varginha exploram diferentes ritmos!", 
+        link: "COMICS/experimento-som-animado.html",
+        coverImage: "image/img10.jpg" // Insira o link/caminho da imagem aqui. Ex: "assets/capa1.png"
+    },
+    { 
+        id: 2, 
+        title: "Desenhando!", 
+        author: "Pâmela de Castro", 
+        category: "quadro a quadro", 
+        color: "FFDE00", 
+        action: "SLASH!", 
+        synopsis: "Peguem seus materiais e vamos criar os desenhos dos nossos próprios personagens!", 
+        link: "COMICS/experimento-tutorial.html",
+        coverImage: "image/comic2/comic-capa.jpg" 
+    },
+    { 
+        id: 3, 
+        title: "ARTBOOK", 
+        author: "Pâmela de Castro", 
+        category: "metatirinhas", 
+        color: "111111", 
+        action: "NULL!", 
+        synopsis: "Uma visita aos bastidores do Metacomics! Conheça o Artbook de Pâmela de Castro.", 
+        link: "COMICS/experimento-livro.html",
+        coverImage: "image/comic3/capa.png"
+    },
+    { 
+        id: 4, 
+        title: "Um novo Olhar", 
+        author: "Pâmela de Castro", 
+        category: "bilu e varginha", 
+        color: "111111", 
+        action: "WOW!", 
+        synopsis: "Varginha apresenta a Bilu um novo olhar sobre o planeta Terra", 
+        link: "COMICS/experimento-desenho.html",
+        coverImage: "image/img7.jpg"
+    },
+    { 
+        id: 5, 
+        title: "Sob o Sol", 
+        author: "Pâmela de Castro", 
+        category: "caminhos", 
+        color: "111111", 
+        action: "WOW!", 
+        synopsis: "Um programador júnior fica preso dentro de um laço de repetição onde seus piores medos são renderizados a 60fps.", 
+        link: "COMICS/experimento-tempo.html",
+        coverImage: "image/img5.jpg"
+    },
+     { 
+        id: 6, 
+        title: "Misturando!", 
+        author: "Pâmela de Castro", 
+        category: "metatirinhas", 
+        color: "111111", 
+        action: "POW!", 
+        synopsis: "Clique e arraste os quadros para mudar a ordem dos acontecimentos do menino das estrelas", 
+        link: "COMICS/experimento-arrastar.html",
+        coverImage: "image/comic6/capa-comic-3.jpg"
+    },
+     { 
+        id: 7, 
+        title: "Segredos", 
+        author: "Pâmela de Castro", 
+        category: "metatirinhas", 
+        color: "111111", 
+        action: "HA!", 
+        synopsis: "Encontre as histórias escodidas na tira interativa", 
+        link: "COMICS/experimento-mapa.html",
+        coverImage: "image/img11.jpg"
+    },
+    { 
+        id: 8, 
+        title: "O Criar", 
+        author: "Pâmela de Castro", 
+        category: "quadro a quadro", 
+        color: "111111", 
+        action: "WOW!", 
+        synopsis: "Uma reflexão interativa sobre o processo de criar arte", 
+        link: "COMICS/experimento-animacao.html",
+        coverImage: "image/img9.jpg"
+    },
+    { 
+         id: 9, 
+        title: "Memórias", 
+        author: "Pâmela de Castro", 
+        category: "quadro a quadro", 
+        color: "111111", 
+        action: "POW!", 
+        synopsis: "De onde vem as inspirações para criar histórias?", 
+        link: "COMICS/experimento-raiox.html",
+        coverImage: "image/comic9/quadro3.png"
+    },
+    {   id: 10, 
+        title: "Música", 
+        author: "Pâmela de Castro", 
+        category: "caminhos", 
+        color: "111111", 
+        action: "POW!", 
+        synopsis: "Prepare a voz ou busque um instrumento musical: O Show já vai começar", 
+        link: "COMICS/experimento-audio.html",
+        coverImage: "image/comic10/quadro1.jpg"
+    },
+    {   id: 11, 
+        title: "O Garra", 
+        author: "Pâmela de Castro", 
+        category: "bilu e varginha", 
+        color: "111111", 
+        action: "POW!", 
+        synopsis: "Chegou a ora de ajudar Bilu e Varginha em sua primeira abdução", 
+        link: "COMICS/experimento-garra.html",
+        coverImage: "image/comic11/img3.jpg"
+    },
+    {   id: 12, 
+        title: "Bom Amigo", 
+        author: "Pâmela de Castro e Balão Mágico", 
+        category: "bilu e varginha", 
+        color: "111111", 
+        action: "POW!", 
+        synopsis: "Adaptação em quadrinhos da canção - É tão lindo - do grupo musical balão mágico ", 
+        link: "COMICS/experimento-trilha-sonora.html",
+        coverImage: "image/comic12/capa.jpg"
+    }
+
+
+        
+
+    
+    /* ⬇️ COPIE O BLOCO ABAIXO PARA ADICIONAR UMA NOVA HQ ⬇️
+    { 
+        id: 4, 
+        title: "Nome da Sua Nova HQ", 
+        author: "Seu Nome", 
+        category: "Ação", 
+        color: "FF3366", // Cor hexadecimal para o fundo caso não tenha capa
+        action: "BAM!", 
+        synopsis: "Escreva a sinopse da sua HQ aqui.", 
+        link: "link-da-pagina.html",
+        coverImage: "caminho-da-imagem.jpg" 
+    }
+    */
+];
